000f88e09f5a89b2599fcfa4c7bd9b697c58cfec
Edevan-PC\Edevan <edevanamaral@gmail.com>
2018-03-12 21:01:15 -0300

    LayoutResponsivo,Clima

--- layoutResponsivo/app/src/main/java/br/grupointegrado/tads/layoutresponsivo/MainActivity.kt
+++ layoutResponsivo/app/src/main/java/br/grupointegrado/tads/layoutresponsivo/MainActivity.kt
@@ -1,19 +1,12 @@
-package br.grupointegrado.tads.meusfavoritos
+package br.grupointegrado.tads.layoutresponsivo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        for (favorito in Favoritos.getLista()){
-            Texto.append("$favorito \n\n\n");
-        }
     }
-
-
 }
