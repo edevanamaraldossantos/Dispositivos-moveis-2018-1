be2ccb22aee1f633936de43d12d20b6f4325ce8e
Edevan-PC\Edevan <edevanamaral@gmail.com>
2018-03-23 21:09:42 -0300

    Criando uma AsynkTask

--- BuscadorGitHoub/app/src/main/java/br/grupointegrado/tads/buscadorgithoub/MainActivity.kt
+++ BuscadorGitHoub/app/src/main/java/br/grupointegrado/tads/buscadorgithoub/MainActivity.kt
@@ -2,14 +2,18 @@ package br.grupointegrado.tads.buscadorgithoub
 
 import android.content.Context
 import android.net.Network
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +39,31 @@ class MainActivity : AppCompatActivity() {
         tv_url.text = urlBuscaNoGithub.toString()
 
         if(urlBuscaNoGithub != null){
-            var resultado = NetworkUtils.obterRespostaDaUrlHtpp(urlBuscaNoGithub)
-            tv_github_resultado.text = resultado
+            //var resultado = NetworkUtils.obterRespostaDaUrlHttp(urlBuscaNoGithub)
+            //tv_github_resultado.text = resultado
+
+            val task = GithubBuscaTask();
+            task.execute(urlBuscaNoGithub)
         }
 
     }
 
+    inner class GithubBuscaTask : AsyncTask<URL, Void, String>() {
+
+        override fun doInBackground(vararg params: URL?): String? {
+            try {
+                var url = params[0]
+                var resultado = NetworkUtils.obterRespostaDaUrlHttp(url!!)
+                return resultado
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            tv_github_resultado.text = result
+        }
+    }
 
 }
