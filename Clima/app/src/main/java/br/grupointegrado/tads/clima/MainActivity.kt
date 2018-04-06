73b96151fb7ea26512a4d2679e4147eeb7cbd820
Edevan-PC\Edevan <edevanamaral@gmail.com>
2018-04-06 00:08:38 -0300

    Interpretando o Json

--- Clima/app/src/main/java/br/grupointegrado/tads/clima/MainActivity.kt
+++ Clima/app/src/main/java/br/grupointegrado/tads/clima/MainActivity.kt
@@ -1,33 +1,30 @@
 package br.grupointegrado.tads.clima
 
+import android.app.Service
 import android.net.Network
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ArrayAdapter
 import br.grupointegrado.tads.buscadorgithub.NetworkUtils
 import br.grupointegrado.tads.clima.dados.ClimaPreferencias
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import org.w3c.dom.Text
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-        /* val dadosClimaFicticios = listOf("Hoje - Céu limpo - 17°C / 15°C",
+    /* val dadosClimaFicticios = listOf(
+                  "Hoje - Céu limpo - 17°C / 15°C",
                  "Amanhã - Nublado - 19°C / 15°C",
                  "Quinta - Chuvoso - 30°C / 11°C",
-                 "Sexta - Chuva com raios - 21°C / 9°C",
-                 "Sábado - Chuva com raios - 16°C / 7°C",
-                 "Domingo - Chuvoso - 16°C / 8°C",
-                 "Segunda - Parcialmente nublado - 15°C / 10°C",
-                 "Ter, Mai 24 - Vai curintia - 16°C / 18°C",
-                 "Qua, Mai 25 - Nublado - 19°C / 15°C",
-                 "Qui, Mai 26 - Tempestade - 30°C / 11°C",
-                 "Sex, Mai 27 - Furacão - 21°C / 9°C",
-                 "Sáb, Mai 28 - Meteóro - 16°C / 7°C",
-                 "Dom, Mai 29 - Apocalipse - 16°C / 8°C",
-                 "Seg, Mai 30 - Pós-apocalipse - 15°C / 10°C")*/
-
+                 "Sexta - Chuva com raios - 21°C / 9°C")*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +33,6 @@ class MainActivity : AppCompatActivity() {
         /*   for (clima in dadosClimaFicticios){
             dados_clima.append("$clima \n\n\n")
         }
-
     */
         carregarDadosDoClima()
     }
@@ -47,33 +43,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == R.id.acao_refresh){
-            tv_dados_clima.text =""
+        if (item?.itemId == R.id.acao_refresh) {
+          //  tv_dados_clima.text = ""
             carregarDadosDoClima()
             return true
         }
         return super.onOptionsItemSelected(item)
     }
 
-    fun exibirResultado(){
+    fun exibirResultado() {
         tv_dados_clima.visibility = View.VISIBLE
         tv_mensagem_erro.visibility = View.INVISIBLE
         pb_aguarde.visibility = View.INVISIBLE
     }
 
-    fun exibirMensagemErro(){
+
+    fun exibirMensagemErro() {
         tv_dados_clima.visibility = View.INVISIBLE
         tv_mensagem_erro.visibility = View.VISIBLE
         pb_aguarde.visibility = View.INVISIBLE
     }
 
-    fun exibirProgressBar(){
+    fun exibirProgressBar() {
         tv_dados_clima.visibility = View.INVISIBLE
         tv_mensagem_erro.visibility = View.INVISIBLE
         pb_aguarde.visibility = View.VISIBLE
     }
 
-    fun carregarDadosDoClima( ){
+    fun carregarDadosDoClima() {
         val localizacao = ClimaPreferencias.getLocalizacaoSalva(this)
         buscarClimaTask().execute(localizacao)
     }
@@ -88,11 +85,11 @@ class MainActivity : AppCompatActivity() {
 
             try {
                 val localizacao = params[0]
-                val  url = NetworkUtils.construirUrl(localizacao)
+                val url = NetworkUtils.construirUrl(localizacao)
 
                 if (url != null) {
                     val resultado = NetworkUtils.obterRespostaDaUrlHttp(url)
-                    return  resultado
+                    return resultado
                 }
 
             } catch (ex: Exception) {
@@ -101,15 +98,48 @@ class MainActivity : AppCompatActivity() {
             return null
         }
 
-        override fun onPostExecute(result: String?) {
 
-            if (result != null){
-                tv_dados_clima.text = result
+       /* override fun onPostExecute(result: String?) {
+
+           if (result != null){
+               tv_dados_clima.text = result
+               exibirResultado()
+           }else{
+             exibirMensagemErro()
+           }
+          }*/
+
+
+        override fun onPostExecute(resultado: String?) {
+            if (resultado != null) {
+
+                val json = JSONObject(resultado)
+                val items = json.getJSONArray("list")
+
+                for (i in 0 until items.length()) {
+                    val obj = items.getJSONObject(i)
+                    val atribDataRecibido = obj.getString("dt")
+                    val dataHoraMilissegundos: Long = (java.lang.Long.valueOf(atribDataRecibido)) * 1000
+                    val dataHora = Date(dataHoraMilissegundos)
+                    val atribDataFormatada = DateFormat.format("dd/MM/yyyy ", dataHora)
+                    val atribMain = obj.getJSONObject("main")
+                    val atribTemp = atribMain.getString("temp")
+                    val atribUmidade = atribMain.getString("humidity")
+
+                    val atri_clima = obj.getJSONArray("weather")
+
+                    val clima = atri_clima.getJSONObject(0)
+                    val descricao = clima.getString("description")
+
+                    tv_dados_clima.append(" Data -> $atribDataFormatada \n"
+                            + " Temperatura -> $atribTemp \n"
+                            + " Umidade -> $atribUmidade \n" +
+                            " Clima-> $descricao \n\n\n")
+                }
                 exibirResultado()
-            }else{
-              exibirMensagemErro()
+            } else {
+                exibirMensagemErro()
             }
         }
     }
-
 }
--- ControledeMetasPessoais/app/src/main/java/br/grupointegrado/tads/controledemetaspessoais/MainActivity.kt
+++ ControledeMetasPessoais/app/src/main/java/br/grupointegrado/tads/controledemetaspessoais/MainActivity.kt
@@ -1,12 +1,114 @@
 package br.grupointegrado.tads.controledemetaspessoais
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //val dadosteste =  listOf("Edevan","Jessica","vanusa")
+    var dados = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val listView = findViewById<ListView>(R.id.lv_metas)
+        // listView.adapter = MyCustomAdpeter(this)
+
+        val adaptar = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, dados)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.opcao, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.acao_visualizar){
+            visualizar()
+        }else if(item?.itemId == R.id.acao_inserir){
+            inserir()
+        }else if(item?.itemId == R.id.acao_editar){
+            editar()
+        }else if(item?.itemId == R.id.acao_excluir){
+            excluir()
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
+
+    fun visualizar(){
+        for ( dados in dados){
+            if(dados != ""){
+               // lv_metas.append("$dados \n")
+
+            }else{
+                Toast.makeText(this,"Não há Registro de metas Pessoais",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun inserir(){
+        val dat = edit_data.text.toString()
+        val tit = edit_titulo.text.toString()
+        val descri = edit_Descricao.text.toString()
+
+        if (dat == "" ){
+            Toast.makeText(this,"Informe a data Prevista",Toast.LENGTH_LONG).show()
+        }else if (tit == ""){
+            Toast.makeText(this,"Informe o Titulo de sua Meta",Toast.LENGTH_LONG).show()
+        }else if (descri ==""){
+            Toast.makeText(this,"Informe a Descrição de sua meta",Toast.LENGTH_LONG).show()
+        }else{
+          dados.add(dat+" "+tit+" "+descri)
+        }
+
+    }
+
+    fun editar(){
+        Toast.makeText(this,"Editar",Toast.LENGTH_SHORT).show()
+    }
+
+    fun excluir(){
+        Toast.makeText(this,"Excluir",Toast.LENGTH_SHORT).show()
+    }
+
+    private class MyCustomAdpeter(context : Context) : BaseAdapter(){
+
+        private val mcontext: Context
+
+        init {
+            this.mcontext = context
+        }
+
+        override fun getCount(): Int {
+            return 1
+        }
+
+        override fun getItemId(position: Int): Long {
+            return  position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+            return "Teste String"
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val textView = TextView(mcontext)
+            textView.text = "Edevan e o cara"
+            return  textView
+        }
+
     }
 }
