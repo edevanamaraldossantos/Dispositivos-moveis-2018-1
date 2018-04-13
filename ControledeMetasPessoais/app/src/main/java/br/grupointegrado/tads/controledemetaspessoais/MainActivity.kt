95568b3a892d0d095c51781a5369a25c5a90e0a2
Edevan-PC\Edevan <edevanamaral@gmail.com>
2018-04-13 13:18:25 -0300

    Finalização do ControledeMetasPessoais

--- ControledeMetasPessoais/app/src/main/java/br/grupointegrado/tads/controledemetaspessoais/MainActivity.kt
+++ ControledeMetasPessoais/app/src/main/java/br/grupointegrado/tads/controledemetaspessoais/MainActivity.kt
@@ -1,41 +1,38 @@
 package br.grupointegrado.tads.controledemetaspessoais
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.*
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     var listV: ListView? = null
-    private var dados = mutableListOf<Metas>()
-    private var adaptar: ArrayAdapter<Metas>? = null
+    private var dados = mutableListOf<Meta>()
     private var posicao = -1
-
+    private var pos = -1
     var dataInicial: String = ""
     var dataFinal: String = ""
     var titulo: String = ""
     var descricao: String = ""
-    var situacao: String = ""
-
+    var situacao: String = "A"
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        val adaptar = ArrayAdapter<Metas>(this, android.R.layout.simple_list_item_1, dados)
-        listV = findViewById<ListView>(R.id.lv_list_item)
-        listV?.setAdapter(adaptar)
+        listV = findViewById(R.id.lv_list_item)
 
-        listV?.setOnItemClickListener {parent, view, position, id ->
-            posicao = position }
+        listV?.setOnItemClickListener { parent, view, position, id ->
+            pos = position
+            situacaoMeta()
+        }
 
-        listV?.setOnCreateContextMenuListener(View.OnCreateContextMenuListener {
-            contextMenu, view, contextMenuInfo -> menuInflater.inflate(R.menu.menu_modificar, contextMenu) })
+        listV?.setOnCreateContextMenuListener(View.OnCreateContextMenuListener { contextMenu, view, contextMenuInfo -> menuInflater.inflate(R.menu.menu_modificar, contextMenu) })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -52,12 +49,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onContextItemSelected(item: MenuItem?): Boolean {
 
-        if(item?.itemId == R.id.acao_editar) {
+        val menuList = item?.getMenuInfo() as AdapterView.AdapterContextMenuInfo
+        posicao = menuList.position
+
+        if (item?.itemId == R.id.acao_editar) {
             retornarSelecionado()
         } else if (item?.itemId == R.id.acao_excluir) {
             excluir()
         } else if (item?.itemId == R.id.acao_finalizar) {
-            Toast.makeText(this, "Finalizar", Toast.LENGTH_LONG).show()
+            finalizar()
         }
         return super.onContextItemSelected(item)
     }
@@ -71,15 +71,22 @@ class MainActivity : AppCompatActivity() {
         } else if (descricao == "") {
             Toast.makeText(this, "Informe a Descrição de sua meta", Toast.LENGTH_LONG).show()
         } else if (dataInicial != "" && titulo != "" && descricao != "") {
-            val meta = Metas(dataInicial.toString(), dataFinal.toString(),
+
+            val meta = Meta(dataInicial.toString(), dataFinal.toString(),
                     titulo.toString(), descricao.toString(), situacao.toString())
-            if (posicao == -1) {
-                dados.add(meta)
-            }else{
-                dados[posicao] = meta
+
+            var verificarData = validarData(dataInicial)
+
+            if (verificarData != null) {
+                if (posicao == -1) {
+                    dados.add(meta)
+                    //Toast.makeText(this, "adiciou!!!", Toast.LENGTH_LONG).show()
+                } else {
+                    dados[posicao] = meta
+                    //Toast.makeText(this, " alterou!!!", Toast.LENGTH_LONG).show()
+                }
             }
-            limparCampos()
-            adaptar?.notifyDataSetChanged()
+            atualizarCampos()
         }
     }
 
@@ -94,22 +101,102 @@ class MainActivity : AppCompatActivity() {
         dataInicial = edit_data.text.toString()
         titulo = edit_titulo.text.toString()
         descricao = edit_descricao.text.toString()
+        edit_data.requestFocus()
     }
 
     fun excluir() {
         dados.removeAt(posicao)
         limparCampos()
-        adaptar?.notifyDataSetChanged()
-
-        //implementar zerar index
+        atualizarCampos()
+        //Toast.makeText(this, "Removeu!!!", Toast.LENGTH_LONG).show()
     }
 
     fun finalizar() {
+        val data = pegarData()
+        dataFinal = pegarData().toString()
+        situacao = "F"
+        retornarSelecionado()
+        inserir()
+        atualizarCampos()
+        Toast.makeText(this, "Finalizou!!!", Toast.LENGTH_LONG).show()
     }
 
     fun limparCampos() {
         edit_data.setText("")
         edit_titulo.setText("")
         edit_descricao.setText("")
+        situacao = "A"
+        dataFinal = ""
+    }
+
+    fun atualizarCampos() {
+        val adaptar = ArrayAdapter<Meta>(this, android.R.layout.simple_list_item_1, dados)
+        listV?.setAdapter(adaptar)
+        adaptar?.notifyDataSetChanged()
+        posicao = -1
+        limparCampos()
+    }
+
+    fun pegarData(): CharSequence {
+        val data = Date()
+        val dataFormatada = android.text.format.DateFormat.format("dd/MM/yyyy ", data)
+        return dataFormatada
+    }
+
+    fun validarData(data: String): Date? {
+        val sdf = SimpleDateFormat("dd/MM/yyyy")
+        sdf.setLenient(false)
+        var dataAgora: Date = Date()
+        var dt = dataAgora
+
+        try {
+            dataAgora = sdf.parse(data)
+            if (vericarData(data)) {
+                return dataAgora
+            } else {
+                Toast.makeText(this, "Verifique os dados -> Dia/Mês/Ano!!!", Toast.LENGTH_SHORT).show();
+                return null
+            }
+        } catch (ex: Exception) {
+            Toast.makeText(this, " Não é Data!!!", Toast.LENGTH_SHORT).show();
+            return null
+        }
+    }
+
+    fun vericarData(data: String): Boolean {
+        val sdf = SimpleDateFormat("dd/MM/yyyy")
+        var dt = Date()
+        dt = sdf.parse(data)
+        val dataAgora = Calendar.getInstance().time
+        var tpDia = (24 * 60 * 60 * 1000)
+        val difDia = ((dt!!.time - dataAgora.time) / tpDia)
+
+        if (difDia < 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    fun situacaoMeta() {
+        val atual = dados[pos]
+        var sit = atual.situacao
+        var tpDia = (24 * 60 * 60 * 1000)
+        var dt = validarData(atual.dataInicial)
+        val dataAgora = Calendar.getInstance().time
+        var difDia = (((dt!!.time - dataAgora.time) / tpDia) + 1)
+        pos = -1
+
+        if (sit != "F") {
+            if (difDia < 0) {
+                Toast.makeText(this, "Meta não comprida!!!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "falta (" + difDia + ") Dia para o Encerramento!!!", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Meta Finalizada!!!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+
--- ControledeMetasPessoais/app/src/main/java/br/grupointegrado/tads/controledemetaspessoais/Meta.kt
+++ ControledeMetasPessoais/app/src/main/java/br/grupointegrado/tads/controledemetaspessoais/Meta.kt
@@ -1,8 +1,6 @@
 package br.grupointegrado.tads.controledemetaspessoais
 
-import android.widget.ListAdapter
-
-class Metas constructor(dataInicial : String, dataFinal: String, titulo: String, descricao : String, situacao : String) {
+class Meta constructor(dataInicial : String, dataFinal: String, titulo: String, descricao : String, situacao : String) {
 
     var dataInicial : String
     var dataFinal : String
@@ -12,16 +10,16 @@ class Metas constructor(dataInicial : String, dataFinal: String, titulo: String,
 
     init {
         this.dataInicial = dataInicial
-        this.dataFinal = dataFinal
         this.titulo = titulo
         this.descricao = descricao
+        this.dataFinal = dataFinal
         this.situacao = situacao
     }
 
     override fun toString(): String {
-        return "\nData de lançamento = $dataInicial \nData de Finalizada = $dataFinal\n" +
-                "Titulo = $titulo\n Descricao = $descricao\n" +
-                " Situção = $situacao\n"
+        return "\nData de lançamento = $dataInicial \nData de Finalizada = $dataFinal\n"+
+                "Titulo = $titulo\n Descricao = $descricao\n"+
+                "Situação =$situacao\n"
     }
 
 }
